bd2c34151ed6450460989b5cd44664dacf589b56
Asam <abbasaliaboubakar@gmail.com>
2025-03-16 16:20:47 +0100

    remove unused vars

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,3 @@
-import cn from "clsx";
 import Document, { Head, Html, Main, NextScript } from "next/document";
 import { DefaultSeo } from "next-seo";
 import { SITE_DESCRIPTION, SITE_NAME } from "@/starter/config";
