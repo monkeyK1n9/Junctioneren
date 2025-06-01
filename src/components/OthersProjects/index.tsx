087c712232ca1962cbd41cdff8f919ac93c3f363
Asam <abbasaliaboubakar@gmail.com>
2025-06-01 07:09:05 +0100

    feat: section other projects

--- src/components/OthersProjects/index.tsx
+++ src/components/OthersProjects/index.tsx
@@ -2,10 +2,11 @@ import Image from "next/image";
 import Menu from "../ui/Menu";
 import { CommonText } from "../ui/Work";
 import images from "../../../public/imgs";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 const OthersProjects = () => {
   return (
-    <section className="py-[20px] lg:py-[46px] lg:max-w-[80%] lg:mx-auto">
+    <section className="pt-[20px] pb-[40px] lg:pb-[200px] lg:pt-[46px] lg:max-w-[80%] lg:mx-auto">
       <div className="container mx-auto">
         <Menu
           title="Other projects"
@@ -13,41 +14,101 @@ const OthersProjects = () => {
           path="#"
           show
         />
-        <CommonText className="lg:mt-[41px] text-center lg:text-left">
-          <span className="mx-4">Interior design</span> Conception, Construction
-          dans la ville de Yaoundé
+        <CommonText className="mt-[20px] lg:mt-[36px] text-center lg:text-left flex flex-col lg:flex-row items-center">
+          <span className="mr-2 text-[#808080]">Interior design</span>
+          <span className="hidden lg:flex">
+            <FaLongArrowAltRight className="mr-2 text-[#808080]" />
+          </span>
+          Conception, Construction dans la ville de Yaoundé
         </CommonText>
-        <div className="my-[101px]">
+        <div className="mt-[40px]">
           <div className="h-[300px] lg:h-[405px] relative">
             <Image
-              src={images.Card4Img}
+              src={images.OtherPic1}
               alt="picture"
               className={"h-[300px] lg:h-[405px] object-cover rounded-md"}
               layout="fill"
             />
           </div>
-          <div className="grid grid-cols-1 mt-[30px] lg:grid-cols-2 justify-center items-center gap-[30px]">
+          <div className="grid grid-cols-1 mt-[30px] lg:grid-cols-2 justify-center items-center gap-[20px]">
             <Image
-              src={images.Card4Img}
+              src={images.OtherPic2}
               alt="picture"
               className={"h-[300px] lg:h-[355px] object-cover rounded-md"}
             />
             <Image
-              src={images.Card4Img}
+              src={images.OtherPic3}
               alt="picture"
               className={"h-[300px] lg:h-[355px] object-cover rounded-md"}
             />
           </div>
-          <div className="grid grid-cols-1 mt-[30px] lg:grid-cols-2 justify-center items-center gap-[30px]">
+          <div className="grid grid-cols-1 mt-[30px] lg:grid-cols-2 justify-center items-center gap-[20px]">
             <Image
-              src={images.Card4Img}
+              src={images.OtherPic4}
               alt="picture"
               className={"h-[300px] lg:h-[255px] object-cover rounded-md"}
             />
             <Image
-              src={images.Card4Img}
+              src={images.OtherPic5}
               alt="picture"
               className={"h-[300px] lg:h-[255px] object-cover rounded-md"}
+            />
+          </div>
+        </div>
+        <CommonText className="mt-[20px] lg:mt-[36px] text-center lg:text-left flex flex-col lg:flex-row items-center">
+          <span className="mr-2 text-[#808080]">Interior design</span>
+          <span className="hidden lg:flex">
+            <FaLongArrowAltRight className="mr-2 text-[#808080]" />
+          </span>
+          Our concepts
+        </CommonText>
+        <div className="py-[40px]">
+          <div className="grid grid-cols-12 justify-center items-center gap-[20px]">
+            <Image
+              src={images.OtherPic6}
+              alt="picture"
+              className={
+                "h-[300px] sm:h-[400px] lg:h-[550px] object-cover rounded-md col-span-12 lg:col-span-8"
+              }
+            />
+            <Image
+              src={images.OtherPic7}
+              alt="picture"
+              className={
+                "h-[300px] sm:h-[400px] lg:h-[550px] object-cover rounded-md col-span-12 lg:col-span-4"
+              }
+            />
+          </div>
+          <div className="grid grid-cols-12 mt-[30px] justify-center items-center gap-[20px]">
+            <Image
+              src={images.OtherPic8}
+              alt="picture"
+              className={
+                "h-[300px] sm:h-[300px] object-cover rounded-md col-span-12 lg:col-span-7"
+              }
+            />
+            <Image
+              src={images.OtherPic9}
+              alt="picture"
+              className={
+                "h-[300px] sm:h-[300px] object-cover rounded-md col-span-12 lg:col-span-5"
+              }
+            />
+          </div>
+          <div className="grid grid-cols-12 mt-[30px] justify-center items-center gap-[20px]">
+            <Image
+              src={images.OtherPic10}
+              alt="picture"
+              className={
+                "h-[300px] sm:h-[300px] object-cover rounded-md col-span-12 lg:col-span-6"
+              }
+            />
+            <Image
+              src={images.OtherPic11}
+              alt="picture"
+              className={
+                "h-[300px] sm:h-[300px] object-cover rounded-md col-span-12 lg:col-span-6"
+              }
             />
           </div>
         </div>
--- src/components/ui/Work.tsx
+++ src/components/ui/Work.tsx
@@ -33,7 +33,7 @@ export const Work = ({ description, title }: WorkProps) => {
   return (
     <div className="rounded-[10px] py-[26px] px-[20px] flex flex-col gap-[10px] border-[1px] border-[#BDBDBD] lg:w-[320px] xl:w-[310px] lg:h-[127px]">
       <h4
-        className={`text-[#312783] ${fontClassRoboto} uppercase font-bold tracking-[20%] leading-[12px] text-[16px] lg:w-[306px]`}
+        className={`text-[#312783] ${fontClassRoboto} uppercase font-bold tracking-[20%] leading-[18px] sm:leading-[12px] text-[16px] lg:w-[306px]`}
       >
         {title}
       </h4>
