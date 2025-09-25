310f73890f58d43d16c886f053f6466957b00e64
King <mkeyk1n9@gmail.com>
2025-09-25 14:28:05 +0200

    Update index.tsx

--- src/pages/our-projects/index.tsx
+++ src/pages/our-projects/index.tsx
@@ -25,11 +25,11 @@ const OurProjectsPage = () => {
             "Un aperçu de nos réalisations dans le secteur de la construction et de l’ingénierie.",
           images: [
             {
-              url: "https://junctioneren.com/favicon.svg",
+              url: "https://junctioneren.com/imgs/contact2.webp",
               width: 1200,
               height: 630,
               alt: "Projets de Junctioner Construction Engineering",
-              type: "image/jpeg",
+              type: "image/webp",
             },
           ],
           siteName: "Junctioner Construction Engineering",
